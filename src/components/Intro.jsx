aadde20ffbaad11b1117608430eea5e6ff47490e
Evan Babic <evanbabic@gmail.com>
2025-02-14 14:01:43 -0500

    worked on jumbotron card with my info

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,19 +1,37 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { GrDocumentPdf } from "react-icons/gr";
+
 function Intro() {
     return (
         <>
-            <div className="container vh-100 d-flex flex-column justify-content-center align-items-center">
-                <div className="row">
-                    <div className="col col-lg-12 text-dark ">
-                        <p className="h4">This is about me!</p>
-                    </div>
-                </div>
+            <div className="card jumbotronStyle noselect">
+                <div className="text-light text-center rgba-stylish-strong py-5 px-4">
+                    <div className="py-5">
+                       <div className="card-title mt-4 pt-2">Evan Babic</div>
+                       <p className="px-md-5 mx-md-5">Computer Science Student</p>
 
-                <div className="row">
-                    <div className="col col-lg-12 text-dark ">
-                        <p className="h4">This is also about me!</p>
+                    <div className="row justify-content-center">
+                        <div className="col-sm-2">
+                            <button className="btn btn-outline">
+                                <FaGithub style={{height: '50px', width: '50px'}} />
+                            </button>
+                        </div>
+                        <div className="col-sm-2">
+                            <button className="btn btn-outline">
+                                <FaLinkedin style={{height: '50px', width: '50px', color: 'blue'}} />
+                            </button>
+                        </div>
+                        <div className="col-sm-2">
+                            <button className="btn btn-outline">
+                                <GrDocumentPdf style={{height: '50px', width: '50px'}} />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
+
+            
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <>
         <nav className="navbar-nav">
-            <div className="container-fluid p-3"> 
+            <div className="container-fluid"> 
                 <div className="row align-items-center">
                     <div className="col-md-8">
                         <a className="navbar-brand" href="#">
@@ -16,13 +16,13 @@ function Navbar() {
                         <div className="d-flex flex-row">
                             <ul className="nav font-large flex-fill justify-content-around">
                                 <li className="nav-item align-self-center">
-                                    <GiSeagull style={{color: 'white', width: '40px', height: '75px'}}/>
+                                    <GiSeagull style={{color: 'white', width: '40px', height: '50px'}}/>
                                 </li>
                                 <li className="nav-item">
-                                    <GiSeaTurtle style={{color: 'white', width: '40px', height: '75px'}}/>
+                                    <GiSeaTurtle style={{color: 'white', width: '40px', height: '50px'}}/>
                                 </li>
                                 <li className="nav-item">
-                                    <GiSeaCreature style={{color: 'white', width: '40px', height: '75px', transform: 'scaleX(-1)'}}/>
+                                    <GiSeaCreature style={{color: 'white', width: '40px', height: '50px', transform: 'scaleX(-1)'}}/>
                                 </li>
                             </ul>
                         </div> 
